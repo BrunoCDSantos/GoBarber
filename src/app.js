e33548ab063cddb5dbe0e4b07c4e3a9335942a2d
Bruno <bruno.coelho.santos17@gmail.com>
2020-02-13 17:47:20 -0200

    criado servidor, conexao com banco de dados

--- src/app.js
+++ src/app.js
@@ -1,20 +1,22 @@
 import express from 'express';
 import route from './routes';
 
+import './database';
+
 class App {
-  constructor() {
-    this.server = express();
-    this.middlewares();
-    this.routes();
-  }
+    constructor() {
+        this.server = express();
+        this.middlewares();
+        this.routes();
+    }
 
-  middlewares() {
-    this.server.use(express.json());
-  }
+    middlewares() {
+        this.server.use(express.json());
+    }
 
-  routes() {
-    this.server.use(route);
-  }
+    routes() {
+        this.server.use(route);
+    }
 }
 
 export default new App().server;
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './App/models/user';
 
 const router = new Router();
 
-router.get('/teste', (req, res) => {
-  return res.json({ message: 'y' });
+router.get('/teste', async (req, res) => {
+    const user = await User.create({
+        name: 'Bruno dos Santos',
+        email: 'bruno.coelho.santos17@gmail.com',
+        password_hash: '11211234',
+    });
+    return res.json({ user });
 });
 
 export default router;
