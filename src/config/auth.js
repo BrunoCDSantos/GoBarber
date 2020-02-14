308d17b677543f712ee3fe40c58357352a947aa8
Bruno <bruno.coelho.santos17@gmail.com>
2020-02-14 17:15:03 -0200

    vai

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+    secrets: '5d0ea65d79d1acc83441923f56ccc89e',
+    expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import UserController from './App/Controllers/UserController';
+import SessionController from './App/Controllers/SessionController';
 
 const router = new Router();
 
 router.post('/users', UserController.store);
+router.post('/users', SessionController.store);
 
 export default router;
