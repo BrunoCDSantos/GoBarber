b716f4cbc3439acdaeb54c86e9fac532920ffd84
Bruno <bruno.coelho.santos17@gmail.com>
2020-02-17 15:38:24 -0300

    criado as autenticacoes das rotas

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,14 @@ import { Router } from 'express';
 import UserController from './App/Controllers/UserController';
 import SessionController from './App/Controllers/SessionController';
 
+import authMiddleware from './App/middlewares/auth';
+
 const router = new Router();
 
 router.post('/users', UserController.store);
-router.post('/users', SessionController.store);
+router.post('/sessions', SessionController.store);
+
+router.use(authMiddleware);
+router.put('/users', UserController.update);
 
 export default router;
